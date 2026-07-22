96070c80a0c0973a5c0677a31b030d19ecac4f7b
Binoy2003 <binoysarker2003@gmail.com>
2026-07-23 02:14:50 +0600

    added array includes

--- includes.js
+++ includes.js
@@ -1,4 +1,8 @@
 const numbers = [23,3,4,56,33,53]
+const number2 = [77,88]
 
 const hasItem = numbers.includes(4);
 console.log(hasItem);
+
+const allNumbers = numbers.concat(number2)
+console.log(allNumbers);

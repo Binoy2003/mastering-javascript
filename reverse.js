c56f21fe74ffeeff71284f9b1af988ebe610971c
Binoy2003 <binoysarker2003@gmail.com>
2026-07-23 20:27:10 +0600

    added array reverse

--- reverse.js
+++ reverse.js
@@ -2,3 +2,12 @@ const numbers = [23,3,4,56,33,53]
 
 const reversed = numbers.reverse()
 console.log(reversed);
+
+const fruits = ['Mango', 'Lychee', 'grape']
+const afterReverse = [];
+
+for(const fruit of fruits){
+  console.log(fruit);
+  afterReverse.unshift(fruit)
+  console.log(afterReverse);
+}

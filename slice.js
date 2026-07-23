5c7109a265169778672c0c0491e018f4dfff1af0
Binoy2003 <binoysarker2003@gmail.com>
2026-07-23 20:37:20 +0600

    added array slice

--- slice.js
+++ slice.js
@@ -3,3 +3,7 @@ const numbers = [2, 3, 5 ,1, 9, 8, 6];
 const slice = numbers.slice(2, 6)
 console.log(slice);
 console.log(numbers);
+
+const parts = numbers.splice(2, 4, 99);
+console.log(parts);
+console.log(numbers);

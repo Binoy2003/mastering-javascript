2b7f08f78f29eaff2bbd0ed9b208354668b9ed34
Binoy2003 <binoysarker2003@gmail.com>
2026-07-23 23:41:55 +0600

    added array comparison

---
+++ comparison.js
@@ -0,0 +1,11 @@
+const subject = 'chemistry';
+
+const book = 'Chemistry';
+
+console.log(book.toLowerCase());
+
+if(subject === book){
+  console.log('poira poira fatai felbo');
+} else {
+  console.log("aybar ki auto pass ase");
+}

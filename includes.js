b97567043243ce9d2b8e3dafd8f5b92df313da1d
Binoy2003 <binoysarker2003@gmail.com>
2026-07-26 17:25:21 +0600

    added includes

--- includes.js
+++ includes.js
@@ -8,3 +8,18 @@ const allNumbers = numbers.concat(number2)
 console.log(allNumbers);
 
 console.log(Array.isArray([77]));
+
+
+
+const email = 'magic@Messi.com'
+if(email.includes ('.com')){
+  console.log('has dot com');
+}
+
+
+
+const fileName = 'My final final final.pdf'
+
+const twitterHandle = '@elonMask'
+
+console.log(fileName.endsWith('.pdf'));

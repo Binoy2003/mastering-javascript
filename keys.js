61ffb10170452a26778e8b0279bd631958fb9a83
Binoy2003 <binoysarker2003@gmail.com>
2026-07-26 20:17:35 +0600

    added keys

---
+++ keys.js
@@ -0,0 +1,12 @@
+const person = {
+  name: 'Janoyar Ali',
+  profession: 'student',
+  age: 64,
+  isSingle: true
+}
+
+const keys = Object.keys(person);
+console.log(keys);
+
+const values = Object.values(person);
+console.log(values);

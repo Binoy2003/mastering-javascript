303ee69790e3463fff89ae5529dee90d10955111
Binoy2003 <binoysarker2003@gmail.com>
2026-07-26 18:57:42 +0600

    added object

---
+++ object.js
@@ -0,0 +1,13 @@
+const sunglass = {
+  color: 'black',
+  price: 120,
+  brand: 'Ray Ban'
+
+}
+
+const phone = {
+  brand: 'Apple'
+  version: 14,
+  isThreeCamera: false,
+  appsInastalled: ['fb', 'wp', 'coc', 'tws']
+}

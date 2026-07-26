bebb95886a7d4ef7d2dbbeb1fdc2474724392955
Binoy2003 <binoysarker2003@gmail.com>
2026-07-26 20:28:32 +0600

    added nested

---
+++ nested.js
@@ -0,0 +1,12 @@
+const college = {
+  name: 'MIT',
+  address: 'Badda',
+  events: ['21 feb', 'Independence Day', 'Victory Day'],
+  people:{
+   name: 'MC sir',
+   yearsOfExperience: 44,
+   degree: 'SPL'
+  }
+}
+
+console.log(college.people);

19da4477814b944ecbe4f7e142f17769b8e27432
Binoy2003 <binoysarker2003@gmail.com>
2026-07-28 18:51:18 +0600

    added function

--- function.js
+++ function.js
@@ -2,6 +2,7 @@ function fanOnKor(){
   console.log('walk towards the switch');
   console.log('press the switch');
   console.log('come back and do whatever you were doing');
+  console.log('well Done');
 }
 
 fanOnKor()

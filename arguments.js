c9588be2800524650974c8583eb43b9b69d8a383
Binoy2003 <binoysarker2003@gmail.com>
2026-07-29 23:36:54 +0600

    added argu

--- arguments.js
+++ arguments.js
@@ -1,7 +1,14 @@
-function multiply(a,b){
+function multiply(a,b){ // parameters
 const result = a * b;
 console.log('result of multi is:', result);
 
 }
 
-multiply(3,4)
+multiply(3,4) // arguments
+
+function square (number){
+  const result = number * number;
+  console.log('square of the number provided', result);
+}
+
+square(8);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+function multiply(a,b){ // parameters
+// const result = a * b;
+// console.log('result of multi is:', result);
+
+// }

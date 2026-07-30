c08773ee71797f37a0b0aba7df690b8b03578ee2
Binoy2003 <binoysarker2003@gmail.com>
2026-07-30 17:52:40 +0600

    added even_arr

---
+++ even_arr.js
@@ -0,0 +1,10 @@
+const numbers = [12, 23, 34, 45, 56, 66];
+
+for(const num of numbers){
+  console.log(num);
+  if(num%2 === 0){
+    console.log('even', num);
+  }else{
+    console.log("odd");
+  }
+}

bb3e466cd411d9e440f1d499143326f6d3eeb478
Binoy2003 <binoysarker2003@gmail.com>
2026-08-01 23:26:11 +0600

    problem 01

--- problem_solve/problem_1.js
+++ problem_solve/problem_1.js
@@ -8,3 +8,7 @@ function checkEvenOdd(number){
 }
 
 console.log(checkEvenOdd(20));
+console.log(checkEvenOdd(45636253));
+console.log(checkEvenOdd(55));
+console.log(checkEvenOdd(49));
+console.log(checkEvenOdd(67));

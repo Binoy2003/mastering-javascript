c90ce2307f933a1b688c355f76ceb0ddfbd16077
Binoy2003 <binoysarker2003@gmail.com>
2026-08-03 02:09:03 +0600

    added problem 8

--- problem_solve/problem_8.js
+++ problem_solve/problem_8.js
@@ -1,5 +1,10 @@
 // count words in a Sentence
 
 function wordCount(str){
-  
+ 
+  let strSplitArr = str.split(" ");
+  return strSplitArr.length;
+
 }
+
+console.log(wordCount("The man who died standing"));

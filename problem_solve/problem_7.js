8aa2cc3359dbb01ba7f82291c84070134ed872e6
Binoy2003 <binoysarker2003@gmail.com>
2026-08-03 01:51:51 +0600

    added problem 7

--- problem_solve/problem_7.js
+++ problem_solve/problem_7.js
@@ -1,12 +1,40 @@
 // check if a String is a Palindrome
 
-let str = 'madam';
+// let str = 'madam';
 
-let reverseStr = '';
+// let reverseStr = '';
 
-for(let i = str.length -1; i>=0; i--){
-  let letter = str[i];
-  reverseStr += letter;
+// for(let i = str.length -1; i>=0; i--){
+//   let letter = str[i];
+//   reverseStr += letter;
+// }
+
+// console.log(reverseStr);
+
+// if( str == reverseStr){
+//   console.log('Palindrome');
+
+// }else{
+//   console.log('Not Palindrome');
+// }
+
+// with function
+
+function isPalindrome(str) {
+  let reverseStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    let letter = str[i];
+    reverseStr += letter;
+  }
+
+  console.log(reverseStr);
+
+  if (str == reverseStr) {
+    console.log("Palindrome");
+  } else {
+    console.log("Not Palindrome");
+  }
 }
 
-console.log(reverseStr);
+console.log(isPalindrome('civic'));

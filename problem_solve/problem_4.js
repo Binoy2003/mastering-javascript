02d07a9382dc15efb6e8a1325dad022928ec1116
Binoy2003 <binoysarker2003@gmail.com>
2026-08-02 22:41:10 +0600

    added problem 4

--- problem_solve/problem_4.js
+++ problem_solve/problem_4.js
@@ -4,8 +4,35 @@
 // divisible by both -- FizzBuzz
 
 
-let number = 7;
+// let number = 7;
 
-for (let i = 1; i<=7; i++){
-  console.log('i ==> ', i);
+// for (let i = 1; i<=7; i++){
+//   console.log('i ==> ', i);
+//   if( i % 3 === 0){
+//     console.log('Fizz');
+//   }else if{
+//     if( i % 5 === 0){
+//     console.log('Buzz');
+//   }else if{
+//     if( i % 3 === 0 && i % 5 === 0){
+//     console.log('FizzBuzz');
+//   }else{
+//     console.log('i = ', i);
+//   }
+// }
+
+
+
+let number = 15;
+
+for (let i = 1; i <= number; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }

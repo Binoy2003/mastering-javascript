521f7cb2c586ac08029dc27eec1252085fc41f7e
Binoy2003 <binoysarker2003@gmail.com>
2026-08-03 13:25:06 +0600

    added problem 9

---
+++ problem_solve/problem_9.js
@@ -0,0 +1,12 @@
+// find largest number in an array
+
+let arr = [500, 120, 34, 445, 1325, 5808]
+
+let largest = arr[0];
+
+for(let i = 1; i < arr.length; i++){
+  
+  let currentElement = arr[i];
+
+  console.log('current Element', currentElement);
+}

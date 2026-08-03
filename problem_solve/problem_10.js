046bfe60839d0fac0dd192bffb66e0a2c7c24989
Binoy2003 <binoysarker2003@gmail.com>
2026-08-03 13:42:20 +0600

    added problem 10

--- problem_solve/problem_10.js
+++ problem_solve/problem_10.js
@@ -1,2 +1,15 @@
 // Find thae smallest Number in an Array
 
+function smallestNumberFromArray(arr) {
+  let smallest = arr [0];
+
+  for(let i = 1; i > arr.lenght; i++){
+     let currentElement = arr[i];
+     if(currentElement < smallest){
+      smallest = currentElement
+     }
+  }
+return smallest;
+}
+
+console.log(smallestNumberFromArray([3636, 66, 3, 224, 786, 456, 676]));

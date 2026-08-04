a07fc49f13e9281ecf6d2666232390c592c3c715
Binoy2003 <binoysarker2003@gmail.com>
2026-08-05 01:53:16 +0600

    added problem 15

--- problem_solve/problem_15.js
+++ problem_solve/problem_15.js
@@ -1,8 +1,34 @@
 // return all even numbers, Then Their Sum
 
-let arr = [10, 20, 50, 150, 6, 7, 9, 11, 23];
+// let arr = [10, 20, 50, 150, 6, 7, 9, 11, 23];
 
-let evenNumbers = [];
+// let evenNumbers = [];
+
+// for(let i = 0; i<arr.length; i++){
+//   console.log(arr[i]);
+
+//   if(arr[i] % 2 === 0){
+//     console.log(arr[i]);
+//     evenNumbers.push(arr[i]);
+//   }
+// }
+
+// console.log(evenNumbers);
+
+// let sum = 0;
+
+// for(let key of evenNumbers){
+//   console.log(key);
+//   sum += key;
+// }
+
+// console.log(sum);
+
+// with Function
+
+function getEvenNumber(arr){
+
+  let evenNumbers = [];
 
 for(let i = 0; i<arr.length; i++){
   console.log(arr[i]);
@@ -12,16 +38,5 @@ for(let i = 0; i<arr.length; i++){
     evenNumbers.push(arr[i]);
   }
 }
-
-console.log(evenNumbers);
-
-let sum = 0;
-
-for(let key of evenNumbers){
-  console.log(key);
-  sum += key;
+return evenNumbers;
 }
-
-console.log(sum);
-
-// 

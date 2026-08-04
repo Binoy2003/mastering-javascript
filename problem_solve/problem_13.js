4ed0f438d50818c3c6dbc4a99508d7c2e831fc28
Binoy2003 <binoysarker2003@gmail.com>
2026-08-04 23:18:20 +0600

    added problem 13

---
+++ problem_solve/problem_13.js
@@ -0,0 +1,12 @@
+// Loop Through an object's Propertise
+
+let monitor = {
+  color: 'Black',
+  brand: 'LG',
+  display: 'Something',
+  size: 'Something',
+  test: 'Something'
+};
+
+console.log(monitor.brand);
+console.log(monitor["color"]);

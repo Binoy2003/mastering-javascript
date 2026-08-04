2fd1acb1a5470a7f4695b1cea267816b54e4a80c
Binoy2003 <binoysarker2003@gmail.com>
2026-08-05 01:47:46 +0600

    added problem 15

--- problem_solve/problem_15.js
+++ problem_solve/problem_15.js
@@ -4,4 +4,11 @@ let arr = [10, 20, 50, 150, 6, 7, 9, 11, 23];
 
 let evenNumbers = [];
 
-for(let i = 0; i<arr)
+for(let i = 0; i<arr.length; i++){
+  console.log(arr[i]);
+
+  if(arr[i] % 2 === 0){
+    console.log(arr[i]);
+    evenNumbers.push(arr[i]);
+  }
+}

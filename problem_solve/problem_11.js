571724d8c3ca51d78d66fd6311ea71e137b16841
Binoy2003 <binoysarker2003@gmail.com>
2026-08-04 19:17:13 +0600

    added problem 11

--- problem_solve/problem_11.js
+++ problem_solve/problem_11.js
@@ -36,3 +36,26 @@ console.log(generateAvg([5, 10, 46, 58, 99, 77]));
 
 
 console.log(generateAvg([10, 20, 30, 40, 50, 60, 70, 80, 90]));
+
+function generateAvg(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+
+    sum += arr[i];
+  }
+
+  console.log(sum);
+  let avg = sum / arr.length;
+
+  return avg;
+}
+
+console.log(generateAvg([5, 10, 15]));
+
+
+console.log(generateAvg([5, 10, 46, 58, 99, 77]));
+
+
+console.log(generateAvg([10, 10, 10]));

05931c18ec3a17d4ae1c2436ff3c8e1ac0247c8e
Binoy2003 <binoysarker2003@gmail.com>
2026-08-04 19:27:52 +0600

    added problem 12

--- problem_solve/problem_12.js
+++ problem_solve/problem_12.js
@@ -1,16 +1,32 @@
 // filter Number Greater Than a value
 
-let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-let value = 45;
+// let value = 45;
 
-let filteredArray = [];
+// let filteredArray = [];
 
-for(let i = 0; i < arr.length; i++){
-  console.log(arr[i]);
-  if(arr[i] > value){
-    filteredArray.push(arr[i]);
+// for(let i = 0; i < arr.length; i++){
+//   console.log(arr[i]);
+//   if(arr[i] > value){
+//     filteredArray.push(arr[i]);
+//   }
+// }
+
+// console.log(filteredArray);
+
+// with function
+
+function getFilteredArray(arr, value) {
+  let filteredArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    if (arr[i] > value) {
+      filteredArray.push(arr[i]);
+    }
   }
+  return filteredArray;
 }
 
-console.log(filteredArray);
+console.log(getFilteredArray([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]), 55);

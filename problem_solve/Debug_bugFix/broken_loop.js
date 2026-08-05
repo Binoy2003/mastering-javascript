35b8eba4da4e63adfa51063ace88a49b748a1ac5
Binoy2003 <binoysarker2003@gmail.com>
2026-08-05 22:52:45 +0600

    added Debuging

--- problem_solve/Debug_bugFix/broken_loop.js
+++ problem_solve/Debug_bugFix/broken_loop.js
@@ -1,10 +1,12 @@
 function sumRange(n) {
+  let total = 0;
   for (let i = 1; i < n; i++) {
     console.log('i-> ', i);
-    let total = 0;
+    // let total = 0;
     total += i;
-    return total;
+    
   }
+  return total;
 }
 
 console.log(sumRange(5));

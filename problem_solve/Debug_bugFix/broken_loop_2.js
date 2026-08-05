5b69c0822cbb6a0c1ae957c3c54d584d53b1eb10
Binoy2003 <binoysarker2003@gmail.com>
2026-08-05 22:58:24 +0600

    added Debuging

---
+++ problem_solve/Debug_bugFix/broken_loop_2.js
@@ -0,0 +1,10 @@
+function sumArray(number){
+  let total = 0;
+
+  for(let i = 0; i <= number.length; i++){
+    total += number[i].tofixed(2);
+  }
+  return total;
+}
+
+console.log(sumArray(10, 20, 2244, 30.524));

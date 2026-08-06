bd37eeaff9d17a213e573c525107f8712a74ad5c
Binoy2003 <binoysarker2003@gmail.com>
2026-08-07 01:25:42 +0600

    Add Assignment 4 solutions

--- Assignment_2/problem_3.js
+++ Assignment_2/problem_3.js
@@ -1,23 +1,21 @@
 function calculateAiCost(tokensUsed) {
-
   if (typeof tokensUsed !== "number" || tokensUsed < 0) {
     return "Invalid";
   }
- 
-if(tokensUsed <= 500){
-  return 0;
+
+  if (tokensUsed <= 500) {
+    return 0;
+  }
+  let extraTokens = tokensUsed - 500;
+
+  let chargeAmount = Math.floor(extraTokens / 100);
+
+  return chargeAmount * 5;
 }
-let extraTokens = tokensUsed - 500;
 
-let chargeAmount = Math.floor(extraTokens / 100);
-
-return chargeAmount * 5;
-
-}
-
-console.log(calculateAiCost(300));    // 0
-console.log(calculateAiCost(500));    // 0
-console.log(calculateAiCost(650));    // 5
-console.log(calculateAiCost(1000));   // 25
-console.log(calculateAiCost(-10));    // Invalid
-console.log(calculateAiCost("500"));  
+console.log(calculateAiCost(300)); // 0
+console.log(calculateAiCost(500)); // 0
+console.log(calculateAiCost(650)); // 5
+console.log(calculateAiCost(1000)); // 25
+console.log(calculateAiCost(-10)); // Invalid
+console.log(calculateAiCost("500"));

52c34d6e2b0d91e28c0572ceddc16503b78b6329
Binoy2003 <binoysarker2003@gmail.com>
2026-08-07 16:04:36 +0600

    Added ES6

--- .vscode/arrow_function.js
+++ .vscode/arrow_function.js
@@ -1,13 +1,18 @@
-function sayGreet(name){
-  return  `Hello ${name}, Good Morning`;
-}
+// function sayGreet(name){
+//   return  `Hello ${name}, Good Morning`;
+// }
 
 // Arrow function syntax
 // const sayGreet = ()=>{
   // return "something"
 //}
 
+const sayGreet = (name) =>{
+  return  `Hello ${name}, Good Morning`;
+}
 
 
 
 console.log(sayGreet("Rivu"));
+
+// console.log(syntax("Messi"));

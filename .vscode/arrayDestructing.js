6184e6281c387f5a8282ea7f885f8ec93cd6372c
Binoy2003 <binoysarker2003@gmail.com>
2026-08-07 20:51:07 +0600

    Added ES6

--- .vscode/arrayDestructing.js
+++ .vscode/arrayDestructing.js
@@ -1,5 +1,18 @@
-const numbers = [10,20,30,40,50]
+const numbers = [10, 20, 30, 40, 50];
 
- const [ten, twenty, , , fifty] = numbers;
+const [ten, twenty, , , fifty] = numbers;
 
- console.log(ten, twenty, fifty);
+//console.log(ten, twenty, fifty);
+
+const student = {
+  name: "Binoy",
+  roll: 2999,
+  age: 22,
+};
+
+
+
+const {age, name} = student
+
+
+console.log(age, name);

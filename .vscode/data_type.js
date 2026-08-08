483882e6cda8ef10cf3cbfeca55431439c1c7b9d
Binoy2003 <binoysarker2003@gmail.com>
2026-08-09 01:55:33 +0600

    Added ES6

---
+++ .vscode/data_type.js
@@ -0,0 +1,13 @@
+// primitive : string, num, boolean, undefined, null
+
+
+let name = "Kavish"
+let age = 45
+let isMarried = false
+let salary = null
+let universityResult = undefined
+
+console.log(typeof name, typeof age, typeof isMarried, typeof universityResult, typeof salary);
+
+// Non primitive : function, object and array
+

5a02a42177ab179c9cb32a303ae6dd6c33af5276
Binoy2003 <binoysarker2003@gmail.com>
2026-08-10 02:18:19 +0600

    Added ES6

--- .vscode/callback_function.js
+++ .vscode/callback_function.js
@@ -2,6 +2,14 @@ function registerStudent(cb){
   console.log("User is registering. Need more data.");
 
   cb();
+
+  function UserBasicInfo(){
+    let student = {
+      name: "Binoy",
+      age: 23,
+      id: 0112330701
+    }
+  }
 }
 
 console.log(registerStudent());

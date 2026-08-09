9bec17c5f136c52d004dbc9cdb4a02cc0430f303
Binoy2003 <binoysarker2003@gmail.com>
2026-08-10 02:08:03 +0600

    Added ES6

--- .vscode/closure.js
+++ .vscode/closure.js
@@ -11,3 +11,14 @@ function createCounter (){
 let counter = createCounter ();
 
 console.log(counter ());
+
+
+function cashRegister (){
+  let amount = 0;
+
+  return function(){
+    return amount;
+  }
+}
+
+let coffeeShopCashCounter

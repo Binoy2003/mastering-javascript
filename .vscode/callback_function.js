64a26a25429ea958009633d64f6f401db8c354a2
Binoy2003 <binoysarker2003@gmail.com>
2026-08-10 14:28:01 +0600

    Added ES6

--- .vscode/callback_function.js
+++ .vscode/callback_function.js
@@ -1,16 +1,32 @@
-function registerStudent(cb){
-  // console.log("User is registering. Need more data.");
+// function registerStudent(cb){
+//   // console.log("User is registering. Need more data.");
 
+//   cb();
+
+//   function userBasicInfo(){
+//     let student = {
+//       name: "Binoy",
+//       age: 23,
+//       id: 0112330701
+//     }
+//     console.log(student);
+//   }
+// }
+
+// console.log(registerStudent(userBasicInfo));
+
+function registerStudent(cb) {
   cb();
-
-  function userBasicInfo(){
-    let student = {
-      name: "Binoy",
-      age: 23,
-      id: 0112330701
-    }
-    console.log(student);
-  }
 }
 
-console.log(registerStudent(userBasicInfo));
+function userBasicInfo() {
+  let student = {
+    name: "Binoy",
+    age: 23,
+    id: "0112330701"
+  };
+
+  console.log(student);
+}
+
+registerStudent(userBasicInfo);

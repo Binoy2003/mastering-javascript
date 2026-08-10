8b0e903a37f68d4c8545e4e0a50c5c0087a9231a
Binoy2003 <binoysarker2003@gmail.com>
2026-08-10 14:51:46 +0600

    Added ES6

--- .vscode/map_foreach_filter_find.js
+++ .vscode/map_foreach_filter_find.js
@@ -1,4 +1,4 @@
-let numbers = [2, 4, 6, 8, 10];
+// let numbers = [2, 4, 6, 8, 10];
 
 // let double = []
 
@@ -12,17 +12,33 @@ let numbers = [2, 4, 6, 8, 10];
 // });
 
 
-const double = numbers.map((elem) => elem * 2);
+// const double = numbers.map((elem) => elem * 2);
 
 
-console.log(double);
+// console.log(double);
 
 
 
 // Foreach
 
-let rolls = [10, 12, 34, 30]
+// let rolls = [10, 12, 34, 30]
 
-rolls.forEach((elem, ind, arr)=>{
-  console.log(elem, ind, arr);
+// rolls.forEach((elem, ind, arr)=>{
+//   console.log(elem, ind, arr);
+// });
+
+
+// filter
+
+let prices = [200, 466, 655, 886, 1000];
+
+let expensiveProducts = prices.filter((elem)=>{
+ console.log(elem);
+ 
+ if(elem >= 500){
+  return true
+ }
+  return false
 });
+
+console.log(expensiveProducts);
